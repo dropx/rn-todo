caaf2d7199fa466614fbf374efd6c3433700d6e6
archer.kang <archer.kang@kakaocorp.com>
2019-03-05 22:04:17 +0900

    1. 유저는 TODO 항목을 입력하여 추가할 수 있다. 추가될 때 TODO 내용, 생성 날짜, 최초상태(DONE 여부)가 저장된다.

--- dojae/rnTodo/App.js
+++ dojae/rnTodo/App.js
@@ -7,8 +7,8 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import React, { Component } from 'react';
+import { Platform, StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -17,12 +17,43 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+//유저는 TODO 항목을 입력하여 추가할 수 있다. 추가될 때 TODO 내용, 생성 날짜, 최초상태(DONE 여부)가 저장된다.
+
+export default class App extends Component {
+
+  //todoItem과 todoList를 state 내에서 관리한다.
+  state = { todoList: [], todoItem: { id: undefined, contents: 'new', createAt: undefined, isDone: 'N' } };
+
+  // todoItem에 대한 내용을 입력한다.
+  inputTodo(contents) {
+    this.setState({ todoItem: { id: this.state.todoItem.id, contents, createAt: this.state.todoItem.createAt, isDone: this.state.todoItem.isDone } })
+  }
+
+  //todoItem을 todoList에 추가한다. 
+  addTodo() {
+    const todoItem = {...this.state.todoItem}
+    
+    todoItem.id = Date.now();
+    todoItem.createAt = new Date();
+    this.setState({ todoList: this.state.todoList.concat( todoItem) });
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>{JSON.stringify(this.state.todoList)}</Text>
+        <Text style={styles.welcome}>{JSON.stringify(this.state.todoItem)}</Text>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder="todo에 입력을 해보세요!"
+          onChangeText={(text) => this.inputTodo(text)}
+        />
+        <Button
+          onPress={this.addTodo.bind(this)}
+          title="입력"
+          color="#841584"
+          accessibilityLabel="Learn more about this purple button"
+        />
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
