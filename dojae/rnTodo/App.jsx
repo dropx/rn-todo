a2c769f352017ad183e0e4bddeee228ffa0300d2
knilnissim <knilnissim@gmail.com>
2019-04-06 10:32:50 +0900

    eslint 룰 추가, 파일 확장자 변경

--- dojae/rnTodo/App.jsx
+++ dojae/rnTodo/App.jsx
@@ -8,7 +8,7 @@
  *
  */
 
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
   Platform,
   StyleSheet,
@@ -16,21 +16,21 @@ import {
   View,
   TextInput,
   Button,
-  FlatList
-} from "react-native";
+  FlatList,
+} from 'react-native';
 
 const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
+  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
   android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
+    'Double tap R on your keyboard to reload,\n' +
+    'Shake or press menu button for dev menu',
 });
 
 export default class App extends Component {
   //todoItem과 todoList를 state 내에서 관리한다.
   state = {
     todoList: [],
-    todoItemInput: ""
+    todoItemInput: '',
   };
 
   textInputRef;
@@ -39,7 +39,7 @@ export default class App extends Component {
   updateTodoItemInput(todoItemInput) {
     this.setState({
       ...this.state,
-      todoItemInput
+      todoItemInput,
     });
   }
 
@@ -49,12 +49,12 @@ export default class App extends Component {
       id: Date.now(),
       createAt: new Date(),
       updateAt: new Date(),
-      contents: this.state.todoItemInput
+      contents: this.state.todoItemInput,
     };
 
     this.setState({
-      todoItemInput: "",
-      todoList: this.state.todoList.concat(todoItem)
+      todoItemInput: '',
+      todoList: this.state.todoList.concat(todoItem),
     });
   }
 
@@ -70,8 +70,8 @@ export default class App extends Component {
       todoList: [
         ...todoList.slice(0, itemIndex),
         item,
-        ...todoList.slice(itemIndex + 1)
-      ]
+        ...todoList.slice(itemIndex + 1),
+      ],
     });
   }
 
@@ -81,31 +81,30 @@ export default class App extends Component {
     const todoList = this.state.todoList;
     const nextStateTodoList = [
       ...todoList.slice(0, itemIndex),
-      ...todoList.slice(itemIndex + 1)
+      ...todoList.slice(itemIndex + 1),
     ];
     this.setState({
       ...this.state,
-      todoList: nextStateTodoList
+      todoList: nextStateTodoList,
     });
   }
 
-  renderTodoItem = (item) => (
-    <View
-      key={item.id}
-      style={styles.todoItem}
-    >
+  renderTodoItem = item => (
+    <View key={item.id} style={styles.todoItem}>
       <View style={styles.todoItemToggleButton}>
         <Button
           onPress={() => this.toggleDone(item.id)}
-          title={item.isDone == true ? "☑️" : "⬜"} // check: ☑, uncheck: ⬜
+          title={item.isDone == true ? '☑️' : '⬜'} // check: ☑, uncheck: ⬜
           color="#841584"
         />
       </View>
       <View style={styles.todoItemContents}>
-        <Text 
-        style={styles.todoItemContentsText}
-        ellipsizeMode="tail" 
-        numberOfLines={1}>{item.contents}</Text>
+        <Text
+          style={styles.todoItemContentsText}
+          ellipsizeMode="tail"
+          numberOfLines={1}>
+          {item.contents}
+        </Text>
       </View>
       <View style={styles.todoItemDeleteButton}>
         <Button
@@ -146,12 +145,12 @@ export default class App extends Component {
           {/*this.state.todoList.map(this.renderTodoItem)*/}
           <FlatList
             data={this.state.todoList.sort((a, b) => {
-              if(a.createAt < b.createAt) return 1;
-              if(a.createAt > b.createAt) return -1;
-              if(a.createAt === b.createAt) return 0;
+              if (a.createAt < b.createAt) return 1;
+              if (a.createAt > b.createAt) return -1;
+              if (a.createAt === b.createAt) return 0;
             })}
-            renderItem={({item}) => this.renderTodoItem(item)}
-            keyExtractor={(item) => String(item.id)}
+            renderItem={({ item }) => this.renderTodoItem(item)}
+            keyExtractor={item => String(item.id)}
           />
         </View>
       </View>
@@ -162,58 +161,58 @@ export default class App extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#F5FCFF",
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
     paddingVertical: 50,
-    paddingHorizontal: 10
+    paddingHorizontal: 10,
   },
 
   todoItemInputView: {
     flex: 1,
-    display: "flex",
-    width: "100%",
-    flexDirection: "row"
+    display: 'flex',
+    width: '100%',
+    flexDirection: 'row',
   },
   todoItemTextInput: {
     flex: 4,
     fontSize: 18,
     padding: 10,
-    borderColor: "#999",
+    borderColor: '#999',
     borderWidth: 1,
-    backgroundColor: "white",
-    marginRight: 10
+    backgroundColor: 'white',
+    marginRight: 10,
   },
   todoItemAddButton: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    borderColor: "#999",
-    borderWidth: 1
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#999',
+    borderWidth: 1,
   },
   todoList: {
     marginTop: 10,
-    width: "100%",
-    display: "flex",
-    flex: 9
+    width: '100%',
+    display: 'flex',
+    flex: 9,
   },
   todoItem: {
-    width: "100%",
-    flexDirection: "row",
-    alignItems: "center", 
-    justifyContent: "space-around",
-    marginVertical: 5
+    width: '100%',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    marginVertical: 5,
   },
   todoItemToggleButton: {
-    flex: 1
+    flex: 1,
   },
   todoItemContents: {
-    flex: 4
+    flex: 4,
   },
   todoItemContentsText: {
-    fontSize: 15
+    fontSize: 15,
   },
   todoItemDeleteButton: {
-    flex: 1
-  }
+    flex: 1,
+  },
 });
